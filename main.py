54145ec127cc7b935dac630be1b7444683de97a8
ilya <iborovkov97@yandex.ru>
2023-11-26 00:34:50 +0300

    Hello, world bot

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-from telegram.ext import ApplicationBuilder
+from dotenv.main import os
+from telegram import ForceReply, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+)
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 
-def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-    app.run_polling()
+async def start(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        rf'Привет, {user.mention_html()}!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Памагити!')
+
+
+def main() -> None:
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('help', help_command))
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
 if __name__ == '__main__':
