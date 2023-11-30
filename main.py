73f1829c823036265e0d8645d27a9714f3ffa67a
ilya <iborovkov97@yandex.ru>
2023-11-30 21:42:45 +0300

    Добавил дополнительный параллельный процесс для запуска скедулера

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 from dotenv.main import os
 from telegram import ForceReply, Update
+import multiprocessing
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -24,11 +27,29 @@ async def help_command(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Памагити!')
 
 
-def main() -> None:
+def poll() -> None:
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('help', help_command))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+def schedule() -> None:
+    raise NotImplementedError
+
+
+def main() -> None:
+    """Бот делится на две части.
+
+    poller_process - парсит сообщения пользователей.
+    scheduler_process - запускает сообщения по расписанию.
+    """
+    poller_process = multiprocessing.Process(name='tg_bot_poll', target=poll)
+    scheduler_process = multiprocessing.Process(
+        name='tg_bot_schedule', target=schedule
+    )
+    poller_process.start()
+    scheduler_process.start()
 
 
 if __name__ == '__main__':
